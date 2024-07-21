6aae67285ba17e66572bca7bf1c03e0501c972bd
niswanareddy <98302322+niswanareddy@users.noreply.github.com>
2024-07-21 13:19:20 +0530

    add the string syntax and functions

--- strings1.py
+++ strings1.py
@@ -2,4 +2,9 @@ print("Niswana Reddy .")
 name =  "Niswana"
 surname = "Reddy"
 
-print(f'{name} + " " + {surname}')
+print(f'{name} + " " + {surname}')  #gap between family name and first name 
+
+a = 'inno'
+print(a[3]) #letter at the 3rd position
+print(len(a)) #lngth of the given string
+print(str(a)) #string value of a
