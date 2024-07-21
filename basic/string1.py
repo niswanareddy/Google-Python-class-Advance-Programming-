1302c87fbe1fef88561dbad7dddbf871414e3f50
niswanareddy <98302322+niswanareddy@users.noreply.github.com>
2024-07-21 15:07:15 +0530

    update spring1

--- basic/string1.py
+++ basic/string1.py
@@ -24,8 +24,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if (count >= 10):
+    return "Number of donuts: many"
+  else:
+      return "Number of donuts: " +str(count)
 
 
 # B. both_ends
@@ -34,8 +36,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if (len(s) < 2):
+    return ""
+  else:
+    return s[:2] + s[len(s)-2:]
 
 
 # C. fix_start
@@ -48,8 +52,8 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  first_char = s[:1]
+  return s[:1] + s[1:].replace(first_char, "*")
 
 
 # D. MixUp
@@ -60,8 +64,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  return a.replace(a[:2], b[:2]) + " " + b.replace(b[:2], a[:2])
 
 
 # Provided simple test() function used in main() to print
